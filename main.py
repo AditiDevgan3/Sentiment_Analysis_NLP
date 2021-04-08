e812f56d80d2c26e65d9953f32111f5a55b27f87
AditiDevgan3 <aditi.devgan18@gmail.com>
2021-04-08 18:09:48 +0530

    Modified

--- main.py
+++ main.py
@@ -9,10 +9,110 @@ Created on Mon Apr  5 12:27:55 2021
 #positive_tweets.json: 5000 tweets with positive sentiments
 #tweets.20150430-223406.json: 20000 tweets with no sentiments
 from nltk.corpus import twitter_samples
+from nltk.corpus import stopwords
+from nltk.tag import pos_tag
+from nltk.stem.wordnet import WordNetLemmatizer
+import re, string
 
+stop_words = stopwords.words("english")
 positive_tweets = twitter_samples.strings("positive_tweets.json")
 negative_tweets = twitter_samples.strings("negative_tweets.json")
 text = twitter_samples.strings('tweets.20150430-223406.json')
 tweet_tokens = twitter_samples.tokenized("positive_tweets.json")
 
-#print(tweet_tokens[0])
+#Applying lemmatizer
+def lemmatize_sentence(tokens):
+    lemmatizer = WordNetLemmatizer()
+    lemmatized_sentence = []
+    for word, tag in pos_tag(tokens):
+        if tag.startswith('NN'):
+            pos = 'n'
+        elif tag.startswith('VB'):
+            pos = 'v'
+        else:
+            pos = 'a'
+        lemmatized_sentence.append(lemmatizer.lemmatize(word, pos))
+    return lemmatized_sentence
+
+#print(lemmatize_sentence(tweet_tokens[0]))
+
+#Removing noise 
+
+def remove_noise(tweet_tokens, stop_words = ()):
+
+    cleaned_tokens = []
+
+    for token, tag in pos_tag(tweet_tokens):
+        token = re.sub('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+#]|[!*\(\),]|'\
+                       '(?:%[0-9a-fA-F][0-9a-fA-F]))+','', token)
+        token = re.sub("(@[A-Za-z0-9_]+)","", token)
+
+        if tag.startswith("NN"):
+            pos = 'n'
+        elif tag.startswith('VB'):
+            pos = 'v'
+        else:
+            pos = 'a'
+
+        lemmatizer = WordNetLemmatizer()
+        token = lemmatizer.lemmatize(token, pos)
+
+        if len(token) > 0 and token not in string.punctuation and token.lower() not in stop_words:
+            cleaned_tokens.append(token.lower())
+    return cleaned_tokens
+        
+print(remove_noise(tweet_tokens[0], stop_words))
+
+#Clean all positive and negative tweets
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- resources.py
+++ resources.py
@@ -11,3 +11,4 @@ nltk.download('punkt')
 
 nltk.download('wordnet')
 nltk.download('averaged_perceptron_tagger')
+nltk.download('stopwords')
