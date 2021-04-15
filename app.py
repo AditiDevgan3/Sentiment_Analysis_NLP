16173173d102f5fb3800b5f481ab60c506d94d2d
AditiDevgan3 <aditi.devgan18@gmail.com>
2021-04-15 17:17:50 +0530

    Modified

--- app.py
+++ app.py
@@ -11,7 +11,10 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return render_template('index.html')
-#    return "Hello World!"
+
+@app.route('/sentiment', methods = ['GET','POST'])
+def sentiment():
+    return render_template('sentiment.html')
 
 if __name__ == "__main__":
     app.run()
