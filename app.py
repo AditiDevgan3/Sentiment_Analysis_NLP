72c6248082279062d4c06fb494d04e91624ed545
AditiDevgan3 <aditi.devgan18@gmail.com>
2021-04-16 17:27:41 +0530

    Updated

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ Created on Wed Apr 14 16:41:17 2021
 @author: Aditi Devgan
 """
 
-from flask import Flask, render_template
+from flask import Flask, render_template,request
 app = Flask(__name__)
 
 @app.route('/')
@@ -14,7 +14,21 @@ def hello_world():
 
 @app.route('/sentiment', methods = ['GET','POST'])
 def sentiment():
-    return render_template('sentiment.html')
+    userid = request.form.get('userid')
+    hashtag = request.form.get('hashtag')
+
+    if userid == "" and hashtag == "":
+        error = "Please Enter any one value"
+        return render_template('index.html', error=error)
+    
+    elif userid == "" or hashtag == "":
+        error = "Both entry not allowed"
+        return render_template('index.html', error=error)
+    
+    else:
+        return render_template('sentiment.html')
+    
+
 
 if __name__ == "__main__":
     app.run()
