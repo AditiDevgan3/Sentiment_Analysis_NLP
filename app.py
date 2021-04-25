eaf70cd2ab0dd5501a7d9f85e69ac94233728386
AditiDevgan3 <aditi.devgan18@gmail.com>
2021-04-25 13:11:18 +0530

    NLP and Flask

--- app.py
+++ app.py
@@ -9,7 +9,6 @@ Created on Mon Apr  5 12:27:55 2021
 #positive_tweets.json: 5000 tweets with positive sentiments
 #tweets.20150430-223406.json: 20000 tweets with no sentiments
 from nltk.corpus import twitter_samples
-from nltk.corpus import stopwords
 from nltk.tag import pos_tag
 from nltk.stem.wordnet import WordNetLemmatizer
 import re, string
@@ -19,7 +18,7 @@ from nltk import classify
 from nltk import NaiveBayesClassifier
 from nltk.tokenize import word_tokenize
 
-stop_words = stopwords.words("english")
+stop_words = ["i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours", "yourself", "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its", "itself", "they", "them", "their", "theirs", "themselves", "what", "which", "who", "whom", "this", "that", "these", "those", "am", "is", "are", "was", "were", "be", "been", "being", "have", "has", "had", "having", "do", "does", "did", "doing", "a", "an", "the", "and", "but", "if", "or", "because", "as", "until", "while", "of", "at", "by", "for", "with", "about", "against", "between", "into", "through", "during", "before", "after", "above", "below", "to", "from", "up", "down", "in", "out", "on", "off", "over", "under", "again", "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each", "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than", "too", "very", "s", "t", "can", "will", "just", "don", "should", "now"]
 positive_tweets = twitter_samples.strings("positive_tweets.json")
 negative_tweets = twitter_samples.strings("negative_tweets.json")
 text = twitter_samples.strings('tweets.20150430-223406.json')
--- resources.py
+++ resources.py
@@ -11,4 +11,3 @@ nltk.download('punkt')
 
 nltk.download('wordnet')
 nltk.download('averaged_perceptron_tagger')
-nltk.download('stopwords')
